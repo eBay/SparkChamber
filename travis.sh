1eae014e215a295412278b58aaca8740817e7067
Steve Elliott <stelliott@ebay.com>
2016-09-30 13:47:42 -0700

    - Improved travis.sh script

--- travis.sh
+++ travis.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 
-TEST_CMD="xcodebuild -workspace SparkWorkspace.xcworkspace -scheme SparkChamber -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' build test; xcodebuild -workspace SparkWorkspace.xcworkspace -scheme SparkKit -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' build test"
-
 which -s xcpretty
 XCPRETTY_INSTALLED=$?
 
+SPARKCHAMBER_TEST_CMD="xcodebuild -workspace SparkWorkspace.xcworkspace -scheme SparkChamber -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' build test"
+
 if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
-  eval "${TEST_CMD} | xcpretty"
+  eval "${SPARKCHAMBER_TEST_CMD} | xcpretty"
 else
-  eval "$TEST_CMD"
+  eval "$SPARKCHAMBER_TEST_CMD"
+fi
+
+SPARKKIT_TEST_CMD="xcodebuild -workspace SparkWorkspace.xcworkspace -scheme SparkKit -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' build test"
+
+if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
+  eval "${SPARKKIT_TEST_CMD} | xcpretty"
+else
+  eval "$SPARKKIT_TEST_CMD"
 fi

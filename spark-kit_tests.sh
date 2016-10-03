8ec583c03149ffecfa64bbd377c0d75fdf202b65
Steve Elliott <stelliott@ebay.com>
2016-10-03 09:30:00 -0700

    - Disabled code signing for CI

--- spark-kit_tests.sh
+++ spark-kit_tests.sh
@@ -3,7 +3,7 @@
 which -s xcpretty
 XCPRETTY_INSTALLED=$?
 
-SPARKKIT_TEST_CMD="xcodebuild -workspace SparkWorkspace.xcworkspace -scheme SparkKit -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=10.0' clean build test"
+SPARKKIT_TEST_CMD="xcodebuild -workspace SparkWorkspace.xcworkspace -scheme SparkKit -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=10.0' CODE_SIGN_IDENTITY='' CODE_SIGNING_REQUIRED=NO clean build test"
 
 if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
   eval "${SPARKKIT_TEST_CMD} | xcpretty"
